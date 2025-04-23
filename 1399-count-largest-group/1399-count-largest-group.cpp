b8fe9d4d0fb4a76e86b841956781a18de5c7694b
AasaSingh05 <158080819+AasaSingh05@users.noreply.github.com>
2025-04-23 22:57:34 +0530

    1399-count-largest-group 04-23-2025 Easy 2 ms (49.15%) 8.2 MB (44.07%) .cpp

---
+++ 1399-count-largest-group/1399-count-largest-group.cpp
@@ -0,0 +1,41 @@
+//using a hashmap approach
+class Solution {
+public:
+    int countLargestGroup(int n) {
+        
+        //creating the hash map
+        unordered_map<int, int> hashMap;
+        
+        //making the max val var
+        int maxValue = 0;
+
+        //iterating through the numbers
+        for (int i = 1; i <= n; ++i) {
+            
+            int key = 0, i0 = i;
+            while (i0) {
+                key += i0 % 10;
+                i0 /= 10;
+            }
+            
+            //incrementing the hash with the key val
+            ++hashMap[key];
+
+            //check if the current size is equal or larger than the existing size 
+            maxValue = max(maxValue, hashMap[key]);
+        }
+
+        //to calculate the count of max size arrays
+        int count = 0;
+
+        //iterating through te array
+        for (auto& [KeyVal, value] : hashMap) {
+            if (value == maxValue) {
+                ++count;
+            }
+        }
+
+        //returning the final val
+        return count;
+    }
+};
