7d629e778bd2c1e7b7bf2a559160c5660a19553d
AasaSingh05 <158080819+AasaSingh05@users.noreply.github.com>
2025-05-22 11:23:44 +0530

    Time: 1 ms (14.1%), Space: 30.5 MB (16.94%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        //declaration and initialization
+        int n = nums.size();
+        vector<int> newarr(n,0);
+
+        //manipulation and copying the array
+        for(int i  = 0; i < n; i++){
+            int index = (i+k) % n;
+
+            //copying the array into the new array
+            newarr[index] = nums[i];
+        }
+
+        //setting this as the new array
+        nums = newarr;
+    }
+};
